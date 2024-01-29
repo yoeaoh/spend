4371408c27439c665a1833c246a343e99d518188
yoeaoh <yoeaoh@gmail.com>
2024-01-29 08:20:48 +0300

    started with dashboard

--- src/pages/dashboard.vue
+++ src/pages/dashboard.vue
@@ -1,5 +1,55 @@
 <script lang="ts" setup></script>
 
 <template>
-    <div>dashboard</div>
+    <div class="dashboard">
+        <div class="dashboard__column">
+            <UCard>
+                <template #header> asd </template>
+
+                asd
+
+                <template #footer> asd </template>
+            </UCard>
+        </div>
+
+        <div class="dashboard__column">
+            <UCard>
+                <template #header> asd </template>
+
+                asd
+
+                <template #footer> asd </template>
+            </UCard>
+        </div>
+
+        <div class="dashboard__column">
+            <UCard>
+                <template #header> asd </template>
+
+                asd
+
+                <template #footer> asd </template>
+            </UCard>
+        </div>
+
+        <div class="dashboard__column">
+            <UCard>
+                <template #header> asd </template>
+
+                asd
+
+                <template #footer> asd </template>
+            </UCard>
+        </div>
+    </div>
 </template>
+
+<style lang="scss" scoped>
+.dashboard {
+    @apply grid grid-cols-4;
+
+    &__column {
+        @apply p-2;
+    }
+}
+</style>
